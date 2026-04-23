3d3246a8fb2d6a065cf09ce3dd34520194d9d9f5
nicolas <nicolas.alejandroit@gmail.com>
2026-04-23 09:22:28 -0400

    primer commit

---
+++ ms-docente/src/main/java/com/proyecto/ms_docente/MsDocenteApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.ms_docente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDocenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDocenteApplication.class, args);
+	}
+
+}
