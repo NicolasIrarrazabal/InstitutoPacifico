e1ed2d70131f5924f52ee8f1791a236a9ff0a462
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-04-26 19:39:56 -0400

    feat: adding dependencies to the service

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/AsignaturasService.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/AsignaturasService.java
@@ -1,4 +1,12 @@
 package com.proyecto.ms_asignaturas.service;
 
+import com.proyecto.ms_asignaturas.repository.AsignaturasRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class AsignaturasService {
+
+    private AsignaturasRepository asignaturasRepository;
 }
--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/PrerequisitoService.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/PrerequisitoService.java
@@ -1,4 +1,12 @@
 package com.proyecto.ms_asignaturas.service;
 
+import com.proyecto.ms_asignaturas.repository.PrerequisitoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class PrerequisitoService {
+
+    private PrerequisitoRepository prerequisitoRepository;
 }
