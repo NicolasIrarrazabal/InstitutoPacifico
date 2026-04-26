71c4d4427275c8e0a3a73dd9d4cbd5f2b5a932b1
nicolas irarrazabal <nicolas.alejandroit@gmail.com>
2026-04-26 19:12:36 -0400

    feat: new attributes to the entities Asignatura, Prerequisitos and Credito

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/model/Credito.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/model/Credito.java
@@ -1,4 +1,26 @@
 package com.proyecto.ms_asignaturas.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "creditos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Credito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull(message = "La cantidad de créditos no puede ser nula")
+    @Min(value = 1, message = "La cantidad de créditos debe ser al menos 1")
+    @Max(value = 15, message = "La cantidad de créditos no puede ser mayor a 15")
+    private Integer cantidad;
 }
