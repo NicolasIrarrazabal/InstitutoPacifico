27008677bdaa93add45ad3dd9f5be46a709da0ea
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-04-26 19:39:56 -0400

    feat: adding dependencies to the service

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/controller/PrerequisitoController.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/controller/PrerequisitoController.java
@@ -1,4 +1,12 @@
 package com.proyecto.ms_asignaturas.controller;
 
+import com.proyecto.ms_asignaturas.repository.PrerequisitoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class PrerequisitoController {
+
+    private PrerequisitoRepository prerequisitoRepository;
 }
