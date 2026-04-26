8419ee181de8c698865ec6eca6388797af52ab88
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-04-26 19:43:29 -0400

    feat: adding dependencies to the service

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/CreditoService.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/CreditoService.java
@@ -1,4 +1,12 @@
 package com.proyecto.ms_asignaturas.service;
 
+import com.proyecto.ms_asignaturas.repository.CreditoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class CreditoService {
+
+    private CreditoRepository  creditoRepository;
 }
