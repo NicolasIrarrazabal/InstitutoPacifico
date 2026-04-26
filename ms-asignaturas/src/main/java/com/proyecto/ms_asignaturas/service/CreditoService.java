6d55f34b83d71416909f12d6b5239a7a6745149a
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-04-26 19:54:30 -0400

    feat: adding the functions to CreditoService

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/CreditoService.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/CreditoService.java
@@ -1,12 +1,27 @@
 package com.proyecto.ms_asignaturas.service;
 
+import com.proyecto.ms_asignaturas.model.Credito;
 import com.proyecto.ms_asignaturas.repository.CreditoRepository;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-@AllArgsConstructor
+@Slf4j
+@RequiredArgsConstructor
 public class CreditoService {
 
-    private CreditoRepository  creditoRepository;
+    private final CreditoRepository  creditoRepository;
+
+    public List<Credito> listarTodos(){
+        return creditoRepository.findAll();
+    }
+
+    public Credito guardar(Credito credito){
+        log.info("El credito se guardo, cantidad de creditos: {}", credito.getCantidad());
+        return creditoRepository.save(credito);
+    }
 }
