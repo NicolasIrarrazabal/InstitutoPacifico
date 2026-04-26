c82eb76eccff66b7160d53be221819c300c382fa
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-04-26 19:12:36 -0400

    feat: new attributes to the entities Asignatura, Prerequisitos and Credito

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/model/Prerequisito.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/model/Prerequisito.java
@@ -1,4 +1,26 @@
 package com.proyecto.ms_asignaturas.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "prerequisitos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Prerequisito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "asignatura_id")
+    private Asignatura asignaturaPrincipal;
+
+    @ManyToOne
+    @JoinColumn(name = "prerequisito_id")
+    private Asignatura asignaturaRequisito;
 }
