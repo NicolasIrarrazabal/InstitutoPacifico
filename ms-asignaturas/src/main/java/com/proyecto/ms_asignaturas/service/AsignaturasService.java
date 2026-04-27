c6bc5d2351fcb2d67b5fcd11289a40584c1fe08e
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-04-26 20:04:15 -0400

    feat: adding the functions to AsignaturaService

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/AsignaturasService.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/AsignaturasService.java
@@ -1,12 +1,36 @@
 package com.proyecto.ms_asignaturas.service;
 
+import com.proyecto.ms_asignaturas.model.Asignatura;
 import com.proyecto.ms_asignaturas.repository.AsignaturasRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
-@AllArgsConstructor
+@Slf4j
+@RequiredArgsConstructor
 public class AsignaturasService {
 
     private AsignaturasRepository asignaturasRepository;
+
+    public List<Asignatura> listarTodas(){
+        return asignaturasRepository.findAll();
+    }
+
+    public Asignatura buscarPorId(UUID id){
+        return asignaturasRepository.findById(id).orElseThrow(()-> new RuntimeException("No se encontro la asignatura con el id"));
+    }
+
+    public Asignatura guardar(Asignatura asignatura){
+        log.info("La asignatura se guardo, nombre de la asignatura: {}", asignatura.getNombre());
+        return asignaturasRepository.save(asignatura);
+    }
+
+    public void elimiarPorId(UUID id){
+        log.warn("Se elimino la asignatura");
+        asignaturasRepository.deleteById(id);
+    }
 }
