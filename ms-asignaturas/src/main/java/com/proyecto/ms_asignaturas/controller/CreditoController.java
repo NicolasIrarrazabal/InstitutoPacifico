559f6c3176a59f17750b78d4fc57aaffdab1cbcc
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-04-26 21:04:30 -0400

    feat: adding Credito Controller with the endpoints

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/controller/CreditoController.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/controller/CreditoController.java
@@ -1,11 +1,29 @@
 package com.proyecto.ms_asignaturas.controller;
 
+import com.proyecto.ms_asignaturas.model.Credito;
+import com.proyecto.ms_asignaturas.service.CreditoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/creditos")
 @RequiredArgsConstructor
 public class CreditoController {
+
+    private final CreditoService creditoService;
+
+    @GetMapping()
+    public ResponseEntity<List<Credito>> listar() {
+        return ResponseEntity.ok(creditoService.listarTodos());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Credito> crearCredito(@Valid @RequestBody Credito credito) {
+        return new ResponseEntity<>(creditoService.guardar(credito), HttpStatus.CREATED);
+    }
 }
