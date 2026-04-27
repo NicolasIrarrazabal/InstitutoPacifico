6f7e7195b9c0707568ba8d5ebf4ceb1c523cddaa
nicolas irarrazabal <nicolas.alejandroit@gmail.com>
2026-04-26 20:58:18 -0400

    fix: import to the Prerequisito Repository

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/repository/PrerequisitoRepository.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/repository/PrerequisitoRepository.java
@@ -2,6 +2,7 @@ package com.proyecto.ms_asignaturas.repository;
 
 import com.proyecto.ms_asignaturas.model.Prerequisito;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
