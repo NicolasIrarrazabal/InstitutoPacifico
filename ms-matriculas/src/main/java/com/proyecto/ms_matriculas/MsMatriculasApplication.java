3f77906023fabee6048cc1cd40be82f4920db74c
Nicolas Irarrazabal <awesomexde123@gmail.com>
2026-04-27 13:49:07 -0400

    Fix comment formatting in main method

--- ms-matriculas/src/main/java/com/proyecto/ms_matriculas/MsMatriculasApplication.java
+++ ms-matriculas/src/main/java/com/proyecto/ms_matriculas/MsMatriculasApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsMatriculasApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//
 		SpringApplication.run(MsMatriculasApplication.class, args);
 	}
 
