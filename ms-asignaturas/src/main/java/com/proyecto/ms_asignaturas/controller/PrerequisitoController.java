908996d4b64259bbb62bce88dafc6ef5db2a4e75
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-04-26 21:08:17 -0400

    feat: adding Prerequisito Controller

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/controller/PrerequisitoController.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/controller/PrerequisitoController.java
@@ -1,13 +1,28 @@
 package com.proyecto.ms_asignaturas.controller;
 
+import com.proyecto.ms_asignaturas.model.Prerequisito;
+import com.proyecto.ms_asignaturas.service.PrerequisitoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/prerequisitos")
 @RequiredArgsConstructor
 public class PrerequisitoController {
 
+    private final PrerequisitoService prerequisitoService;
 
+    @GetMapping("/asignatura/{id}")
+    public ResponseEntity<List<Prerequisito>> listarPorAsignatura(@PathVariable UUID id) {
+        return ResponseEntity.ok(prerequisitoService.listarPorAsignatura(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Prerequisito> asignarPrerequisito(@Valid @RequestBody Prerequisito prerequisito) {
+        return ResponseEntity.ok(prerequisitoService.asignarPrerequisito(prerequisito));
+    }
 }
