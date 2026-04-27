2f02a9b8e5138439eafc42c2066dfc13e63517fd
Albedo-AB <di.aravenab@duocuc.cl>
2026-04-27 12:29:28 -0400

    Creacion Repository package y clase EstudianteRepository

---
+++ ms-estudiante/src/main/java/com/proyecto/ms_estudiante/repository/EstudianteRepository.java
@@ -0,0 +1,10 @@
+package com.proyecto.ms_estudiante.repository;
+
+import com.proyecto.ms_estudiante.model.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
+    Optional<Estudiante> findByRut(String rut);
+}
