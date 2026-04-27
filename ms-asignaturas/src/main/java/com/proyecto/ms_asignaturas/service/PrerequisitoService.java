42225364150083ea8836e66a9b452b8e7c0b3948
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-04-26 20:28:23 -0400

    feat: adding the functions to PrerequisitoService

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/PrerequisitoService.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/service/PrerequisitoService.java
@@ -1,12 +1,31 @@
 package com.proyecto.ms_asignaturas.service;
 
+import com.proyecto.ms_asignaturas.model.Prerequisito;
 import com.proyecto.ms_asignaturas.repository.PrerequisitoRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
-@AllArgsConstructor
+@Slf4j
+@RequiredArgsConstructor
 public class PrerequisitoService {
 
     private PrerequisitoRepository prerequisitoRepository;
+
+    public List<Prerequisito> listarPorAsignatura(UUID asignaturaId) {
+        return prerequisitoRepository.findAll().stream()
+                .filter(p -> p.getAsignaturaPrincipal().getId().equals(asignaturaId))
+                .toList();
+    }
+
+    public Prerequisito asignarPrerequisito(Prerequisito prerequisito) {
+                prerequisito.getAsignaturaRequisito().getNombre();
+                prerequisito.getAsignaturaPrincipal().getNombre();
+        return prerequisitoRepository.save(prerequisito);
+    }
+
 }
