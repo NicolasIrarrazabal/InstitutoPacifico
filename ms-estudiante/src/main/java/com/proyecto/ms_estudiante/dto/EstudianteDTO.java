ed5f23c9dad41a321d57464e1ea875206f711276
Albedo-AB <di.aravenab@duocuc.cl>
2026-04-27 12:32:54 -0400

    Creacion service package y clase EstudianteService

--- ms-estudiante/src/main/java/com/proyecto/ms_estudiante/dto/EstudianteDTO.java
+++ ms-estudiante/src/main/java/com/proyecto/ms_estudiante/dto/EstudianteDTO.java
@@ -2,7 +2,11 @@ package com.proyecto.ms_estudiante.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor // Lombok genera el constructor
+@Getter // Lombok genera los getters automáticamente
 public class EstudianteDTO {
 
     @NotBlank
