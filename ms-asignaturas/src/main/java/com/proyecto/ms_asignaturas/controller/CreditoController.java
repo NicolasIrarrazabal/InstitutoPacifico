d8e7e5a02eacd7451e8f8cc26d89d39d38ee4c6f
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-04-26 20:55:01 -0400

    feat: Asignatura Controller finished

--- ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/controller/CreditoController.java
+++ ms-asignaturas/src/main/java/com/proyecto/ms_asignaturas/controller/CreditoController.java
@@ -1,4 +1,11 @@
 package com.proyecto.ms_asignaturas.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/creditos")
+@RequiredArgsConstructor
 public class CreditoController {
 }
