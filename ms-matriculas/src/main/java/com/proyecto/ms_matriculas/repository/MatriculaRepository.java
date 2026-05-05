05c1027fd5b845c0f8d9d73df2a0b1b316765385
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-05-04 20:06:40 -0400

    refactor: actualizar controladores con mejoras en logging y eliminar MatriculaRepository duplicado

--- ms-matriculas/src/main/java/com/proyecto/ms_matriculas/repository/MatriculaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.ms_matriculas.repository;
-
-import com.proyecto.ms_matriculas.model.Matricula;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
-}
