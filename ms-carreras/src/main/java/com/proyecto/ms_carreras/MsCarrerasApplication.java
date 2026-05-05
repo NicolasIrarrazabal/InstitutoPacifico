8bd49cd9ed51dbca30ab3f191f035434689d6176
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-05-04 20:05:25 -0400

    feat: crear microservicio ms-carreras completo

---
+++ ms-carreras/src/main/java/com/proyecto/ms_carreras/MsCarrerasApplication.java
@@ -0,0 +1,12 @@
+package com.proyecto.ms_carreras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCarrerasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCarrerasApplication.class, args);
+    }
+}
