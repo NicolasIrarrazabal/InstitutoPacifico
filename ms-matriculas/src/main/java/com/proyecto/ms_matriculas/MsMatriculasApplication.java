d1c2cbc43e5d87f63b1e8b61d7b397dabbfc7947
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-05-04 20:05:53 -0400

    fix: corregir ubicacion de MatriculaRepository de repository a controller

--- ms-matriculas/src/main/java/com/proyecto/ms_matriculas/MsMatriculasApplication.java
+++ ms-matriculas/src/main/java/com/proyecto/ms_matriculas/MsMatriculasApplication.java
@@ -2,8 +2,10 @@ package com.proyecto.ms_matriculas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsMatriculasApplication {
 
 	public static void main(String[] args) {//
