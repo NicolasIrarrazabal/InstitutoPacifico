3312ea6afe4c1c24f5bbf3354d8546c0d9bfc83f
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-07-02 08:40:46 -0400

    fix: remove hardcoded servers in OpenAPI config to auto-detect URL

--- ms-carreras/src/main/java/com/proyecto/ms_carreras/OpenApiConfig.java
+++ ms-carreras/src/main/java/com/proyecto/ms_carreras/OpenApiConfig.java
@@ -2,11 +2,8 @@ package com.proyecto.ms_carreras;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -18,10 +15,6 @@ public class OpenApiConfig {
                         .title("ms-carreras")
                         .description("Microservicio de gestión de carreras - Instituto Pacífico. " +
                                 "Gestiona el catálogo de carreras disponibles en el instituto.")
-                        .version("1.0.0"))
-                .servers(List.of(
-                        new Server().url("https://institutopacifico-ms-carreras.onrender.com").description("Render (producción)"),
-                        new Server().url("http://localhost:8084").description("Local")
-                ));
+                        .version("1.0.0"));
     }
 }
